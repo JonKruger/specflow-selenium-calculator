f85ba074adf8f5732f1bc08078f93cfcddcb4efe
Jon Kruger <jkruger@igsenergy.com>
2013-11-18 21:58:52 -0500

    code

---
+++ specflow-selenium-calculator/Calculator.feature
@@ -0,0 +1,21 @@
+﻿Feature: Calculator
+    Test a four function calculator
+
+Background:
+    Given I have opened the calculator
+
+Scenario: Add two numbers
+    When I press "123+456="
+    Then the result should be "579"
+
+Scenario: Divide two numbers, result is a fraction
+    When I press "3/2="
+    Then the result should be "1.5"
+
+Scenario: Divide by zero
+    When I press "5/0="
+    Then the result should be "Infinity"
+
+Scenario: Test order of operations
+    When I press "2+3x4="
+    Then the result should be "14"
---
+++ specflow-selenium-calculator/CalculatorSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAutomation;
+using TechTalk.SpecFlow;
+
+namespace specflow_selenium_calculator
+{
+    [Binding]
+    public class CalculatorSteps : FluentAutomation.FluentTest
+    {
+        [Given(@"I have opened the calculator")]
+        public void GivenIHaveOpenedTheCalculator()
+        {
+            FluentAutomation.SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Firefox);
+            FluentAutomation.Settings.DefaultWaitUntilTimeout = new TimeSpan(0, 0, 3);
+            I.Open(new Uri(@"c:\proj\specflow-selenium-calculator\specflow-selenium-calculator\calc.htm", UriKind.Absolute));
+        }
+        
+        [When(@"I press ""(.*)""")]
+        public void WhenIPress(string buttons)
+        {
+            foreach (var ch in buttons)
+                I.Click(string.Format("input[value*='{0}']", ch));
+        }
+        
+        [Then(@"the result should be ""(.*)""")]
+        public void ThenTheResultShouldBe(string result)
+        {
+	    I.Expect.Text(result).In("input[name='Input']");
+        }
+    }
+}
